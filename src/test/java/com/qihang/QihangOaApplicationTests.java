437d8592c3b256b9d481246221724cc2c87cea33
COKID <Bourbon-Conan@outlook.com>
2018-01-10 17:01:39 +0800

    new

---
+++ src/test/java/com/qihang/QihangOaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.qihang;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class QihangOaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
